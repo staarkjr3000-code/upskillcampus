672ed54cc382845f11e635bb5454433295f84160
staarkjr3000-code <staark.jr3000@gmail.com>
2026-03-16 09:38:11 +0530

    Implement basic IoT monitoring system output

---
+++ IoTMonitoringSystem.py
@@ -0,0 +1,9 @@
+print("IoT Based Smart Monitoring System")
+
+temperature = 25
+humidity = 60
+
+print("Temperature:", temperature)
+print("Humidity:", humidity)
+
+print("Data sent to cloud successfully")
